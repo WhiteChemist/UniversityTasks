40c2582783912f949d36399c6f45ebb828af40a5
Pavel Zarembo <zarembo2000@gmail.com>
2019-09-30 23:38:45 +0300

    Task 7 has been completed

--- UniversityTasks/University_Task_7.cpp
+++ UniversityTasks/University_Task_7.cpp
@@ -2,17 +2,25 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include <cstdlib>
 #include <windows.h>
 #include <ostream>
+#include <vector>
 #include <sstream>
 #pragma warning(disable:4996)
 using namespace std;
-char* Surname = new char;
+const double GOODMARK = 7.0;//Средний бал выше которого студенты будут считаться отличниками
+vector<Student>* students;
+Student* student = new Student;
 void WorkWithStudent()
 {
+
 	char path[256];
 	int switcher = 0;
 	bool exit = true;
+	std::cout << "Please input path for work:" << std::endl;
+	std::cin >> path;
+	students = new vector<Student>[GetCountStudents(path)];
 	while (exit)
 	{
 		std::cout << "Make change:" << std::endl;
@@ -20,6 +28,8 @@ void WorkWithStudent()
 		std::cout << "1. Create new student and write him in file." << std::endl;
 		std::cout << "2. Search good students from interesting you group." << std::endl;
 		std::cout << "3. Print all students" << std::endl;
+		std::cout << "4. Fill array students" << std::endl;
+		std::cout << "5. Override path" << std::endl;
 		std::cin >> switcher;
 		switch (switcher)
 		{
@@ -27,29 +37,31 @@ void WorkWithStudent()
 			exit = false;
 			break;
 		case 1:
-			std::cout << "Input path file: " << std::endl;
-			std::cin >> path;
 			AddInFile(CreateStudent(), path);
 			break;
 		case 2:
-			std::cout << "Input path file: " << std::endl;
-			std::cin >> path;
-			FillArrayStudents(path);
+			SearchGoodStudents(path);
 			break;
 		case 3:
-			std::cout << "Input path file: " << std::endl;
-			std::cin >> path;
 			GetAllStudents(path);
 			break;
+		case 4:
+			FillArrayStudents(path);
+			break;
+		case 5:
+			std::cout << "Input new path" << std::endl;
+			std::cin >> path;
+			break;
 		default:
+			delete students;
 			std::cout << "Wrong number!" << std::endl;
 			break;
 		}
 	}
 }
-Student CreateStudent()
+Student CreateStudent()//Функция для заполнения студента
 {
-	char Surname[256];
+	std::string Surname;
 	int marks[3]{ 0,0,0 };
 	int group = 0;
 	std::cout << "Input Surname: " << std::endl;
@@ -62,10 +74,10 @@ Student CreateStudent()
 	std::cin >> marks[1];
 	std::cout << "Input rate computer science: " << std::endl;
 	std::cin >> marks[2];
-	Student student(Surname,group,marks[1],marks[0],marks[2]);
+	Student student(Surname, group, marks[1], marks[0], marks[2]);
 	return student;
 }
-void AddInFile(Student student, char path[])
+void AddInFile(Student student, char path[])//Функция для записи в файл
 {
 	try
 	{
@@ -73,14 +85,13 @@ void AddInFile(Student student, char path[])
 		file.open(path, std::ios::app);
 		if (file.is_open())
 		{
+			file << "Group:" << student.GetGroup() << std::endl;
+			file << "Surname:" << student.GetSurname() << std::endl;
+			file << "CompScience:" << student.GetCS() << std::endl;
+			file << "Math:" << student.GetMath() << std::endl;
+			file << "Phys:" << student.GetPhys() << std::endl;
+			file << "Average:" << student.GetAverageMark() << std::endl;
 			file << "*" << std::endl;
-			file << "Group: " << student.GetGroup() << std::endl;
-			file << "Surname: " << name << std::endl;
-			file << "Average: " << student.GetAverageMark() << std::endl;
-			file << "CompScience: " << student.GetCS() << std::endl;
-			file << "Math: " << student.GetMath() << std::endl;
-			file << "Phys: " << student.GetPhys() << std::endl;
-			file << "*	" << std::endl;
 		}
 		else
 		{
@@ -90,7 +101,7 @@ void AddInFile(Student student, char path[])
 	}
 	catch (const std::exception& ex)
 	{
-		std::cout << ex.what()<<std::endl;
+		std::cout << ex.what() << std::endl;
 	}
 }
 void GetAllStudents(char path[])	//Получение списка студентов из файла
@@ -103,62 +114,82 @@ void GetAllStudents(char path[])	//Получение списка студентов из файла
 		{
 			while (std::getline(file, line))
 			{
-			
-				std::cout << line << std::endl;
-			}
-		}
-		file.close();
-	}
-	catch (std::exception& ex)
-	{
-		std::cout << ex.what() << std::endl;
-	}
-}
-void SearchGoodStudents(int group, std::string path)// Поиск студентов со средним баллом выше 7
-{
-	try
-	{
-		std::string line;
-		std::ifstream file(path);
-		if (file.is_open())
-		{
-			while (std::getline(file, line))
-			{
-				std::cout << line << std::endl;
-			}
-		}
-		file.close();
-	}
-	catch (std::exception& ex)
-	{
-		std::cout << ex.what() << std::endl;
-	}
-}
-void FillArrayStudents(char path[])
-{
-	setlocale(LC_ALL, "Russian");
 
-	char numb[] = { "Surname:" };
-	std::string newf;
-	int counter = 0;
+				std::cout << line << std::endl;
+			}
+		}
+		file.close();
+	}
+	catch (std::exception& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+}
+void SearchGoodStudents(std::string path)// Поиск студентов со средним баллом выше 7
+{
+	try
+	{
+		for (int i = 0; i < (*students).size(); i++)
+		{
+			if ((*students)[i].GetAverageMark() > GOODMARK)
+			{
+				std::cout << "Good student: " << (*students)[i].GetSurname() << ". With average mark:" << (*students)[i].GetAverageMark() << std::endl;
+			}
+		}
+	}
+	catch (std::exception& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+}
+void FillArrayStudents(char path[])//Функция для парсинга из файла данных
+{
+	auto temp = new Student[GetCountStudents(path)];
 	try
 	{
 		std::string line;
 		std::ifstream file(path);
 		if (file.is_open())
 		{
-			while (std::getline(file, line))
+			for (int i = 0; i < GetCountStudents(path); i++)
 			{
-				if (line.find(numb) != std::string::npos)
-				{
-					counter++;
-					newf = line.substr(_countof(numb), line.capacity());
-					std::cout << newf;
-				}
-				else
-				{
-					cout << "Не найдено";
-				}
+				while (std::getline(file, line))
+					if (line.find("*") != std::string::npos)
+					{
+						students->push_back(temp[i]);
+						i++;
+
+					}
+					else if (line.find("Group:") != std::string::npos)
+					{
+						temp[i].SetGroup(std::stoi(line.substr(_countof("Group:") - 1, line.capacity())));//делаем -1 т.к. не считывает первый символ фамилии
+
+					}
+					else if (line.find("Surname:") != std::string::npos)
+					{
+						temp[i].SetSurname(line.substr(_countof("Surname:") - 1, line.capacity()));//делаем -1 т.к. не считывает первый символ фамилии
+
+					}
+					else if (line.find("CompScience:") != std::string::npos)
+					{
+						temp[i].SetCS(std::stoi(line.substr(_countof("CompScience:") - 1, line.capacity())));//делаем -1 т.к. не считывает первый символ фамилии
+
+					}
+					else if (line.find("Math:") != std::string::npos)
+					{
+						temp[i].SetMath(std::stoi(line.substr(_countof("Math:") - 1, line.capacity())));//делаем -1 т.к. не считывает первый символ фамилии
+
+					}
+					else if (line.find("Phys:") != std::string::npos)
+					{
+						temp[i].SetPhys(std::stoi(line.substr(_countof("Phys:") - 1, line.capacity())));//делаем -1 т.к. не считывает первый символ фамилии
+
+					}
+					else if (line.find("Average:") != std::string::npos)
+					{
+						temp[i].SetAverageMark(std::stod(line.substr(_countof("Average:") - 1, line.capacity())));//делаем -1 т.к. не считывает первый символ фамилии
+
+					}
 			}
 			std::cout << std::endl;
 		}
@@ -168,5 +199,29 @@ void FillArrayStudents(char path[])
 	{
 		std::cout << ex.what() << std::endl;
 	}
-	std::cout << counter << std::endl;
+}
+int GetCountStudents(char path[])
+{
+	int count = 0;
+	try
+	{
+		std::string line;
+		std::ifstream file(path);
+		if (file.is_open())
+		{
+			while (std::getline(file, line))
+			{
+				if (line.find("*") != std::string::npos)
+				{
+					count++;
+				}
+			}
+		}
+		file.close();
+	}
+	catch (std::exception& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+	return count;
 }
--- UniversityTasks/University_Task_7.h
+++ UniversityTasks/University_Task_7.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <fstream>
+#include <vector>
 #include <sstream>
 struct Student
 {
@@ -13,12 +14,12 @@ private:
 		int math;
 		int computer_science;
 	};
-	char* Surname;
+	std::string Surname;
 	int number_group;
 	double average_mark;
 	rating rate;
 public:
-	Student(char* surname, int group, int phys, int math, int computer_science)
+	Student(std::string surname, int group, int phys, int math, int computer_science)
 	{
 		this->Surname = surname;
 		this->number_group = group;
@@ -40,31 +41,62 @@ public:
 	{
 		return this->rate.math;
 	}
+	void SetMath(int math)
+	{
+		this->rate.math = math;
+	}
 	int GetPhys()
 	{
 		return this->rate.phys;
+	}
+	void SetPhys(int phys)
+	{
+		this->rate.phys = phys;
 	}
 	int GetCS()
 	{
 		return this->rate.computer_science;
 	}
-	char* GetSurname()
+	void SetCS(int computerscience)
+	{
+		this->rate.computer_science = computerscience;
+	}
+	std::string GetSurname()
 	{
 		return this->Surname;
+	}
+	void SetSurname(std::string surname)
+	{
+		this->Surname = surname;
 	}
 	double GetAverageMark()
 	{
 		return this->average_mark;
 	}
+	void SetAverageMark(double averagemark)
+	{
+		this->average_mark = averagemark;
+	}
+	void CalculateAverageMark()
+	{
+		this->average_mark = (double)(this->rate.computer_science + this->rate.math + this->rate.phys) / 3;
+	}
 	int GetGroup()
 	{
 		return this->number_group;
 	}
+	void SetGroup(int group)
+	{
+		this->number_group = group;
+	}
 
 };
+void SearchGoodStudents(std::string path);
 void WorkWithStudent();
 Student CreateStudent();
 void AddInFile(Student student, char path[]);
 void GetAllStudents(char path[]);
 void FillArrayStudents(char path[]);
+int GetCountStudents(char path[]);
+
 #endif
